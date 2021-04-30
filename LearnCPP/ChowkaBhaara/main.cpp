2b54a76c18eebc788ac17b55c5cb3e253b22d94b
Altair Penubaku <manav.prasad.27@gmail.com>
2021-04-30 23:06:59 +0200

    added while loop to run code block repetitively

--- LearnCPP/ChowkaBhaara/main.cpp
+++ LearnCPP/ChowkaBhaara/main.cpp
@@ -5,48 +5,75 @@
 #include <cstdlib>
 #include <ctime>
 
-int rolldice(){
+int rolldice()
+{
 	int dice_value;
 	dice_value = (rand() % 5) + 1;
-	if(dice_value == 5){
+	if (dice_value == 5)
+	{
 		dice_value = 8;
 	}
 	return dice_value;
 }
 
-int main() {
-	srand((unsigned) time(0));
+int main()
+{
+	srand((unsigned)time(0));
 	int NPlayers;
 	cout << "Enter number of players" << endl;
-	cin >> NPlayers ;
+	cin >> NPlayers;
 	Player User[NPlayers];
-	Player PlayerA ;
+	Player PlayerA;
 	Coins Coin[4];
+	bool end_game;
 
-	for (int i = 0; i < NPlayers; i++) {
+	for (int i = 0; i < NPlayers; i++)
+	{
 		string name;
-		cout << "Enter" << " " <<  i+1 << " " << "player's name" << endl;
+		cout << "Enter"
+			 << " " << i + 1 << " "
+			 << "player's name" << endl;
 		cin >> name;
 		User[i].name = name;
 		cout << "Choose the color RED, BLUE, GREEN OR YELLOW" << endl;
-		cout << "Enter" << " " << i + 1 << " " << " player colour" << endl;
+		cout << "Enter"
+			 << " " << i + 1 << " "
+			 << " player colour" << endl;
 		cin >> name;
 		User[i].colourName = name;
 	}
-	
-	User[0].Coin[0].x = 1;
 
-	char keyboard_input;
-	int ascii_code;
-	cout << "Press r to roll dice" << endl;
-	cin >> keyboard_input;
-	ascii_code = (int)keyboard_input;
-	if(ascii_code == 82 || ascii_code == 114){
-		cout << rolldice() << endl;
+	while (1)
+	{
+		char keyboard_input;
+		int ascii_code;
+		int dice_value;
+		for (int i = 0; i < NPlayers; i++)
+		{
+			cout << User[i].name << " " << "Press r to roll dice" << endl;
+			cin >> keyboard_input;
+			ascii_code = (int)keyboard_input;
+			if (ascii_code == 82 || ascii_code == 114)
+			{
+				dice_value = rolldice();
+				if (dice_value == 4 || dice_value == 8)
+				{
+					User[i].Coin[0].x = 1;
+					User[i].Coin[0].y = 3;
+					cout << "CHOWKA BARA : You can move your pawn out of quarantine" << endl;
+				}
+			}
+			else
+			{
+				cout << "r wasn't entered" << endl;
+				end_game = 1;
+				break;
+			}
+		}
+		if (end_game == 1)
+		{
+			break;
+		}
 	}
-
 	return 0;
 }
-
-
-
