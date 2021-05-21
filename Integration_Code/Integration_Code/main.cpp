93738dc618dfdef53ef86b3bf4a33987704771c4
apandit <apandit@rbionics.com>
2021-05-21 21:38:31 +0200

    board baby!

--- Integration_Code/Integration_Code/main.cpp
+++ Integration_Code/Integration_Code/main.cpp
@@ -54,9 +54,12 @@ int main(int argc, char** argv)
 	sf::Event event;
 
 	sf::CircleShape square(100, 4);
-	sf::RectangleShape sq(Vector2f{ 75,75 });
+	sf::RectangleShape sq(Vector2f{ 100,100 });
+	sf::RectangleShape rectangle1(sf::Vector2f(4, 100*sqrt(2))); // change the size to 100x100 rectangle.setSize(sf::Vector2f(100, 100));
+	sf::RectangleShape rectangle2(sf::Vector2f(4, 100 * sqrt(2)));
 	square.setRotation(45);
-
+	rectangle1.setRotation(-45);
+	rectangle2.setRotation(45);
 
 	while (renderWindow.isOpen())
 	{
@@ -66,13 +69,23 @@ int main(int argc, char** argv)
 		}
 		renderWindow.clear();
 
-		for (int i = 0; i < 7; i++)
+		for (int i = 0; i < 5; i++)
 		{
-			for (int j = 0; j < 7; j++)
+			for (int j = 0; j < 5; j++)
 			{
 				sq.setFillColor(sf::Color(192, 192, 192));
-				sq.setPosition({ (float)(77 * i + 100), (float)(77 * j + 100) });
+				sq.setPosition({ (float)(105 * i + 100), (float)(105 * j + 100) });
 				renderWindow.draw(sq);
+				if  ( (i % 2 == 0 && j % 2 == 0 && (((i + j) == 2) || ((i + j) == 6 ))) || (i==2 && j==2))
+				{
+					rectangle1.setPosition({ (float)(105 * i + 100), (float)(105 * j + 100) });
+					rectangle1.setFillColor(sf::Color(0, 0, 0));
+					renderWindow.draw(rectangle1);
+					rectangle2.setPosition({ (float)(105 * i + 200), (float)(105 * j + 100) });
+					rectangle2.setFillColor(sf::Color(0, 0, 0));
+					renderWindow.draw(rectangle2);
+				}
+
 			}
 				// renderwindow.draw(line ,2, sf::lines);
 		}
