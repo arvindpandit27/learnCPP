e0329c55d0cdde65fcec0cb35d1bf9471ffc99c5
chetan <chetan6491@gmail.com>
2021-05-28 06:35:12 +0200

    Made the borders for squares and also made the triangle to appear on the board

--- Integration_Code/Integration_Code/BoardGraphics.cpp
+++ Integration_Code/Integration_Code/BoardGraphics.cpp
@@ -7,11 +7,14 @@ void BoardGraphics() {
 
 	sf::Event event;
 
-	sf::CircleShape square(100, 4);
+	//sf::CircleShape square(100, 4);
 	sf::RectangleShape sq(Vector2f{ 100,100 });
 	sf::RectangleShape rectangle1(sf::Vector2f(4, 100 * (float)sqrt(2))); // change the size to 100x100 rectangle.setSize(sf::Vector2f(100, 100));
 	sf::RectangleShape rectangle2(sf::Vector2f(4, 100 * (float)sqrt(2)));
-	square.setRotation(45);
+	sf::CircleShape triangle(30, 3);
+
+	//sf::RectangleShape sq(Vector2f{ 100,100 });
+	//square.setRotation(45);
 	rectangle1.setRotation(-45);
 	rectangle2.setRotation(45);
 
@@ -27,16 +30,25 @@ void BoardGraphics() {
 		{
 			for (int j = 0; j < 5; j++)
 			{
-				sq.setFillColor(sf::Color(192, 192, 192));
+				sq.setFillColor(sf::Color(0, 0, 0));
 				sq.setPosition({ (float)(105 * i + 100), (float)(105 * j + 100) });
+				sq.setOutlineThickness(5);
+				sq.setOutlineColor(sf::Color(255, 215, 0));
+
+				triangle.setFillColor(sf::Color(255,0,0));
+				triangle.setPosition({ (float)(100), (float)(100) });
+				//triangle.setOutlineThickness(2);
+				//triangle.setFillColor(sf::Color(0,255,0));
+				renderWindow.draw(triangle);
+
 				renderWindow.draw(sq);
 				if ((i % 2 == 0 && j % 2 == 0 && (((i + j) == 2) || ((i + j) == 6))) || (i == 2 && j == 2))
 				{
 					rectangle1.setPosition({ (float)(105 * i + 100), (float)(105 * j + 100) });
-					rectangle1.setFillColor(sf::Color(0, 0, 0));
+					rectangle1.setFillColor(sf::Color(255, 215, 0));
 					renderWindow.draw(rectangle1);
 					rectangle2.setPosition({ (float)(105 * i + 200), (float)(105 * j + 100) });
-					rectangle2.setFillColor(sf::Color(0, 0, 0));
+					rectangle2.setFillColor(sf::Color(255,215, 0));
 					renderWindow.draw(rectangle2);
 				}
 
